0b8f9c3d88896c9313ab14e453e1df200f06d9a7
Wscthhh <tianhao.chen@sectrend.com.cn>
2022-08-09 16:30:12 +0800

    [新增功能](header首页): 初始实现header搜索功能

--- src/views/home/Header.vue
+++ src/views/home/Header.vue
@@ -1,13 +1,45 @@
 <script setup lang="ts">
-import { menu, system } from './const '
+import { menu, system, search } from './const '
 import { ref } from 'vue'
+import router from '@/router'
+type Isearch = {
+  component: string
+  url: string
+}
 const menuCurrent = ref<number>()
 const systemCurrent = ref<number>()
+const searchListCurrent = ref<number>(0)
+const searchVal = ref<string>('')
+const searchList = ref<Isearch[]>([])
+const searchFlag = ref<boolean>(false)
 const menuClick = (index) => {
   menuCurrent.value = index
 }
 const systemClick = (index) => {
   systemCurrent.value = index
+}
+const searchChange = () => {
+  if (searchVal.value) {
+    searchList.value = search.filter((el) => {
+      return el.component.toLowerCase().split('').includes(searchVal.value.trim().toLowerCase())
+    })
+  } else {
+    searchList.value = []
+  }
+}
+const listEnter = (index) => {
+  searchListCurrent.value = index
+}
+const listActive = (item: Isearch) => {
+  router.push(item.url)
+  searchVal.value = ''
+  searchList.value = []
+}
+const searchBlur = () => {
+  searchFlag.value = false
+}
+const searchFocus = () => {
+  searchFlag.value = true
 }
 </script>
 <template>
@@ -17,7 +49,20 @@ const systemClick = (index) => {
       <div v-for="(item, index) in menu" :key="index" class="menu" @click="menuClick(index)">
         <span :class="{ menuActive: menuCurrent === index }">{{ item }}</span>
       </div>
-      <input type="text" placeholder="搜索" />
+      <div class="searchBox">
+        <input type="text" placeholder="搜索" @input="searchChange" @focus="searchFocus" @blur="searchBlur" v-model="searchVal" />
+        <ul class="list" :class="{ searchBlur: !searchFlag }" v-if="searchList.length !== 0">
+          <li
+            v-for="(item, index) in searchList"
+            :key="index"
+            :class="{ searchListActive: searchListCurrent === index }"
+            @mouseenter="listEnter(index)"
+            @click="listActive(item)"
+          >
+            {{ item.component }}
+          </li>
+        </ul>
+      </div>
     </div>
     <div class="systemBox">
       <div v-for="(item, index) in system" :key="index" class="system" :class="{ systemActive: systemCurrent === index }">
@@ -62,6 +107,11 @@ const systemClick = (index) => {
     }
   }
 }
+.searchBox {
+  display: flex;
+  align-items: center;
+  position: relative;
+}
 input {
   transition: all 0.3s;
   height: 30px;
@@ -81,10 +131,38 @@ input {
     box-shadow: 0 0 0 2px rgba(24, 160, 88, 0.2);
   }
 }
+.list {
+  position: absolute;
+  width: 160px;
+  background-color: #fff;
+  box-shadow: 0 0 1px 1px rgb(226, 226, 230);
+  top: 38px;
+  border-radius: 2px;
+  left: 3px;
+  margin: 0;
+  padding: 5px;
+  li {
+    list-style: none;
+    text-align: left;
+    transition: all 0.3s;
+    cursor: pointer;
+    border-radius: 2px;
+    padding: 5px 5px;
+    &:hover {
+      background-color: pink;
+    }
+  }
+}
 .menuActive {
   color: #18a058;
 }
 .systemActive {
   background-color: rgb(236, 232, 232);
+}
+.searchListActive {
+  background-color: pink;
+}
+.searchBlur {
+  display: none;
 }
 </style>
--- src/views/home/const .ts
+++ src/views/home/const .ts
@@ -1,4 +1,15 @@
 const menu = ['首页', '文档', '组件']
 
 const system = ['English', '深色', 'Github', '0.0.1']
-export { menu, system }
+
+const search = [
+  {
+    component: '按钮 Button',
+    url: '/document/button'
+  },
+  {
+    component: 'Input',
+    url: '/document/input'
+  }
+]
+export { menu, system, search }
