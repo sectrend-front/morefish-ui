a123f53b4c14efec4541da6e2490a324e27e2d63
Wscthhh <tianhao.chen@sectrend.com.cn>
2022-08-10 16:39:03 +0800

    [新增功能](换肤功能): 新增换肤功能

--- src/views/home/const .ts
+++ src/views/home/const .ts
@@ -1,15 +1,7 @@
 const menu = ['首页', '文档', '组件']
 
-const system = ['English', '深色', 'Github', '0.0.1']
+const system = ['English', '深色', 'Github']
 
-const search = [
-  {
-    component: '按钮 Button',
-    url: '/document/button'
-  },
-  {
-    component: 'Input',
-    url: '/document/input'
-  }
-]
-export { menu, system, search }
+const version = '0.0.1'
+
+export { menu, system, version }
