2f4955b84c1bf3274e62c18f99687409dd91198c
zhi.jiang <zhi.jiang@sectrend.com.cn>
2022-11-04 11:29:22 +0800

    [文档修改](doc): 修改文档

--- packages/components.ts
+++ packages/components.ts
@@ -6,3 +6,6 @@ export { default as Tabs, TabPane } from '#c/Tabs'
 export { default as Space } from '#c/Space'
 export { default as Tag } from '#c/Tag'
 export { default as Divider } from '#c/Divider'
+
+// 组件参考
+// https://github.com/57code/sheep-ui/blob/master/src/tree/src/tree-type.ts
